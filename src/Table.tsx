47f8bb70374c77eb621eb44ed5fc0bcbe38dfede
LJGrant <92326859+LJGrant@users.noreply.github.com>
2022-06-16 10:26:29 -0400

    revamp tables to work as single component with lots o props

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,29 +1,68 @@
-import React, { ReactElement, useEffect, useState } from 'react'
-import TableBody from './TableBody'
-import TableHead from './TableHead'
-import TableRow from './TableRow'
-import SearchBar from './SearchBar'
+import React, { useEffect, useState } from 'react'
 
 export interface Item {
   id: string | number
+}
+
+export interface Header {
   label: string
+  slug: string
+}
+
+export interface DisplayProps {
+  action: Function
+}
+
+export interface Action {
+  action: Function
+  label: string
+  classNames: string[]
+}
+
+export interface Styles {
+  tableContainer?: string[]
+  searchBar?: string[]
+  table?: string[]
+  thead?: string[]
+  tbody?: string[]
+  tr?: string[]
+  th?: string[]
+  td?: string[]
 }
 
 type TableProps = {
-  className?: string
-  children: ReactElement
+  styles?: Styles
   items: Item[]
+  headers: Header[]
+  actions?: Action[]
+  search?: Boolean
 }
 
-const Table: React.FC<TableProps> = ({ className, children, items }) => {
+const Table: React.FC<TableProps> = ({
+  styles,
+  items,
+  headers,
+  actions = [],
+  search = false,
+}) => {
+  const [sortedItems, setSortedItems] = useState(items)
   const [masterCheck, setMasterCheck] = useState<boolean>(false)
   const [selectedItems, setSelectedItems] = useState<Item[]>([])
+  const [searchParam, setSearch] = useState('')
+  const [sortParam, setSortParam] = useState({
+    slug: headers[0].slug,
+    direction: '',
+  })
 
   useEffect(() => {
     if (selectedItems.length === 0) {
       setMasterCheck(false)
     }
   }, [selectedItems])
+
+  useEffect(() => {
+    setSortedItems(items)
+  }, [items])
 
   const onItemCheck = async (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -51,23 +90,130 @@ const Table: React.FC<TableProps> = ({ className, children, items }) => {
     }
   }
 
-  const childrenWithProps = React.Children.map(children, (child) => {
-    // taking each child and adding props to it
-    return React.cloneElement(child, {
-      masterCheck,
-      onMasterCheck,
-      selectedItems,
-      setSelectedItems,
-      onItemCheck,
-    })
-  })
+  const sort = (slug: string) => {
+    if (sortParam.slug === slug && sortParam.direction === 'asc') {
+      setSortedItems((prevState) =>
+        prevState.sort((a, b) => {
+          if (a[slug] > b[slug]) return -1
+          if (a[slug] < b[slug]) return 1
+          return 0
+        })
+      )
+      setSortParam({
+        slug,
+        direction: 'desc',
+      })
+    } else if (sortParam.slug === slug && sortParam.direction === 'desc') {
+      setSortedItems((prevState) =>
+        prevState.sort((a, b) => {
+          if (a[headers[0].slug] < b[headers[0].slug]) return -1
+          if (a[headers[0].slug] > b[headers[0].slug]) return 1
+          return 0
+        })
+      )
+      setSortParam({
+        slug: headers[0].slug,
+        direction: '',
+      })
+    } else {
+      setSortedItems((prevState) =>
+        prevState.sort((a, b) => {
+          if (a[slug] < b[slug]) return -1
+          if (a[slug] > b[slug]) return 1
+          return 0
+        })
+      )
+      setSortParam({
+        slug,
+        direction: 'asc',
+      })
+    }
+  }
 
-  return <table className={`${className}`}>{childrenWithProps}</table>
+  return (
+    <div className={styles?.tableContainer?.join(' ')}>
+      <div className={styles?.searchBar?.join(' ')}>
+        {search && (
+          <input
+            className="search-input"
+            value={searchParam}
+            onChange={(e) => {
+              setSearch(e.target.value)
+            }}
+          />
+        )}
+        {actions?.length > 0 &&
+          actions?.map(({ label, action, classNames }, index) => (
+            <button
+              className={classNames?.join(' ')}
+              onClick={() => action(selectedItems)}
+              key={`action-${index}`}
+            >
+              {label}
+            </button>
+          ))}
+      </div>
+      <table className={styles?.table?.join(' ')}>
+        <thead>
+          <tr className={styles?.tr?.join(' ')}>
+            <th className={styles?.th?.join(' ')}>
+              <input
+                type="checkbox"
+                checked={masterCheck}
+                id="mastercheck"
+                onChange={onMasterCheck}
+              />
+            </th>
+            {headers.map((header, index) => (
+              <th
+                key={`header-${index}`}
+                className={styles?.th?.join(' ')}
+                onClick={() => sort(header.slug)}
+              >
+                {header.label}
+                {sortParam.slug === header.slug && (
+                  <>
+                    {sortParam.direction === 'desc' ? (
+                      <>&#x25B2;</>
+                    ) : (
+                      sortParam.direction === 'asc' && <>&#x25BC;</>
+                    )}
+                  </>
+                )}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sortedItems.map((item, index) => (
+            <tr
+              className={styles?.tr?.join(' ')}
+              onClick={() => {}}
+              key={`item-${index}`}
+            >
+              <td className={styles?.td?.join(' ')}>
+                <input
+                  onClick={(e) => e.stopPropagation()}
+                  type="checkbox"
+                  checked={selectedItems.includes(item)}
+                  id="rowcheck{item.id}"
+                  onChange={(e) => onItemCheck(e, item)}
+                />
+              </td>
+              {headers.map(({ slug }, tindex) => (
+                <td
+                  className={styles?.td?.join(' ')}
+                  key={`item-${index}-${tindex}`}
+                >
+                  {item[slug]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
-export default Object.assign(Table, {
-  Body: TableBody,
-  SearchBar: SearchBar,
-  Head: TableHead,
-  Row: TableRow,
-})
+export default Table
